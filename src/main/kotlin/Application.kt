ad2c04a87c7f3bc92633b8252334f87b768c2e7b
aRetr0 <gseguraa@edU.tecnocampus.cat>
2025-10-15 00:32:20 +0200

    install content negotiation with JSON serialization in the application module

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,13 +1,18 @@
 package com.example
 
 import com.example.routes.bookRoutes
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
     configureRouting()
     bookRoutes()
 }
