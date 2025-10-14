b5f38b1782aa5fcd619c32c8c73142a25bb7b4e9
aRetr0 <gseguraa@edU.tecnocampus.cat>
2025-10-15 00:15:10 +0200

    update book routes to handle book addition and return list of books

--- src/main/kotlin/routes/BookRoutes.kt
+++ src/main/kotlin/routes/BookRoutes.kt
@@ -1,18 +1,29 @@
 package com.example.routes
 
+import com.example.models.Book
 import io.ktor.server.application.Application
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 
+val myBooks = mutableListOf<Book>()
+
 fun Application.bookRoutes() {
     routing {
         get("/books") {
-            call.respondText("Listado libros")
+            call.respond(myBooks)
         }
         post("/books") {
-            call.respondText("Libro recibido")
+            try {
+                myBooks.add(call.receive<Book>())
+                call.respondText("Libro añadido")
+            } catch (e: Exception) {
+                call.respondText("No se ha podido añadir: ${e.message}")
+            }
+
         }
     }
 }
